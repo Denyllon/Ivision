f7d4fa65065fb428574844f3a0100d2158afe1c2
Denyllon <daniel.studencki@gmail.com>
2017-07-26 18:01:54 +0200

    initial commit + layout done

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(0);
+
+/***/ })
+],[1]);
+//# sourceMappingURL=main.js.map
